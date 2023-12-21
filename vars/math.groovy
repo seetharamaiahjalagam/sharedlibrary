e1343adc79ad576c4c826212f746943f5f42d614
seetharamaiahjalagam <152842571+seetharamaiahjalagam@users.noreply.github.com>
2023-12-21 12:09:10 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,16 @@
-
+def sum (a,b)
+{
+  echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def sum (a,b)
+{
+  echo "The subscration of ${a} and ${b} is {a-b}"
+}
+def sum (a,b)
+{
+  echo "The product of ${a} and ${b} is {a*b}"
+}
+ def sum (a,b)
+{
+   echo "The divide of ${a} and {b} is {a/b}"
+}
